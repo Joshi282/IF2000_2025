4ecf8a55dd97df5d5494a2d1460300b76ea70df1
Joshana Thompson <joshanajunezthompson@gmail.com>
2025-08-14 11:26:32 -0600

    Mi Primer commit en Java

---
+++ If2000_introduccionprogra/src/if2000_introduccionprogra/If2000_introduccionprogra.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package if2000_introduccionprogra;
+
+/**
+ *
+ * @author josha
+ */
+public class If2000_introduccionprogra {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        String name = "Lionel";
+        int age = 40;
+        System.out.println("Hello word");
+        System.out.println("Name: "+name);
+        System.out.println("Age: "+age);
+        /*
+        Tipos de datos numericos:
+        int, long, short, float, double, numeric, money, decimal
+        
+        manejo de fechas:
+        date
+        
+        Cadena de caracteres:
+        String
+        
+        Objetos:
+        object
+        
+        Data Structutres:
+        Arrays or Vectors: int[] x;
+                           int[][] y;
+        */
+    }
+    
+}
+ 
